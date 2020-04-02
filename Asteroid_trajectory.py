2a82c37e446c4e7f0964dd1e9bccb242aaeb6886
Max Perraudin <max.perraudin@gmail.com>
2020-04-02 15:20:03 +0200

    Fix of arccos input between [-1,1] + Plot of orbital parameters
    - Fixed the error that occurred when calculating arccos(inclination), the values were sometimes slightly over 1.
    - Added the plots of the variations of the orbital parameters.
    - Cleaned and fixed some minor bugs

--- Asteroid_trajectory.py
+++ Asteroid_trajectory.py
@@ -15,12 +15,15 @@ def f(t, y):
     return np.array([y0, y1, y2, y3, y4, y5])
 
 
+# TODO : Influence of Jupiter
+
+
 def RK4(function, time_vector, initial_conditions, h):
     # Definition of the Runge-Kutta method at the order 4
     results = []
     yin = initial_conditions
     results.append(yin)  # set the first value to the initial conditions
-    for t in time_vector:
+    for t in time_vector[1:]:
         k1 = function(t, yin)
         k2 = function(t + h / 2, yin + h / 2 * k1)
         k3 = function(t + h / 2, yin + h / 2 * k2)
@@ -39,6 +42,7 @@ def backward_forward(function, time_vector, initial_conditions, h):
 
 
 def extract_vectors(results):
+    # takes the list of outputs from RK4 and extracts a list of position and velocity vectors
     r_list = []
     r_dot_list = []
     for i in range(len(results)):
@@ -52,24 +56,46 @@ def extract_vectors(results):
     return r_list, r_dot_list
 
 
-def orbital_parameters(r, rdot):  # takes two vectors
-    a = (2 / np.linalg.norm(r) - np.linalg.norm(rdot) ** 2 / mu) ** (-1)
-    e = np.linalg.norm(np.cross(rdot, np.cross(r, rdot)) / mu - r / np.linalg.norm(r))
+def orbital_parameters(r, rdot):
+    # takes two vectors : pos(x,y,z) and v(x,y,z) and computes the orbital parameters at this point
+    semi_major_axis = (2 / np.linalg.norm(r) - np.linalg.norm(rdot) ** 2 / mu) ** (-1)
+    eccentricity = np.linalg.norm(np.cross(rdot, np.cross(r, rdot)) / mu - r / np.linalg.norm(r))
     k_vector = np.cross(r, rdot) / (np.linalg.norm(r) * np.linalg.norm(rdot))
-    i = np.arccos(k_vector[2])
-    return a, e, i
+    inclination = np.arccos(np.around(k_vector[2], 4))  # np.around rounds the value of k_vectors so it stays in [-1,1]
+    return semi_major_axis, eccentricity, inclination
 
 
 def orbital_parameters_list(r, rdot):
+    # takes the list of position vectors and velocity vectors and computes the corresponding
+    # orbital parameters at each point. Returns individual lists of those parameters
     a_list = []
     e_list = []
     i_list = []
-    for iii in range(len(r_list)):
+    for iii in range(len(r)):
         a, e, i = orbital_parameters(r[iii], rdot[iii])
         a_list.append(a)
         e_list.append(e)
         i_list.append(i)
     return a_list, e_list, i_list
+
+
+def orbit_var(a, e, i, t):
+    # Plots the orbital parameters in order to visualise their variations over time
+    plt.figure(1)
+    plt.plot(t, a, color='red')
+    plt.title('Variations of the Semi-major Axis [a]')
+    plt.figure(2)
+    plt.plot(t, e, color='blue')
+    plt.title('Variations of the Eccentricity [e]')
+    plt.figure(3)
+    plt.plot(t, i, color='green')
+    plt.title('Variations of the Inclination [i]')
+    plt.show()
+
+
+# =============================================================================================
+# Definition of the initial parameters
+# =============================================================================================
 
 # We use our own units : distances in AU, mass in Suns, time in Days
 
@@ -89,24 +115,31 @@ tf = 1033  # final time
 ti = 0  # starting time
 step = 1  # step wanted
 
-time = np.arange(ti, tf, step)  # creation of the list containing each value of time
+time = np.arange(ti, tf + step, step)  # creation of the list containing each value of time
+
+
+# =============================================================================================
+# Start of the computation
+# =============================================================================================
 
 forward, backward = backward_forward(f, time, init_state, step)
 
 # plot of the trajectory
 for i in range(len(forward)):
-    plt.plot(forward[i][0], forward[i][1], 'o', color='red')
+    plt.plot(forward[i][0], forward[i][1], 'o', color='red', markersize=1)
 plt.axis('equal')
+plt.title('Trajectory around the sun')
 plt.show()
 
 # computation of the error between forward and backward
 err_x = 150e6 * abs(forward[0][0] - backward[-1][0])
 err_y = 150e6 * abs(forward[0][1] - backward[-1][1])
-print(err_x, err_y)
+print(' Error on x : ', err_x, ' km\n', 'Error on y : ', err_y, ' km')
+
 # TODO: Add automatic step adjustment in function of the error
 
 r_list, rdot_list = extract_vectors(forward)
 
 a_list, e_list, i_list = orbital_parameters_list(r_list, rdot_list)
 
-
+orbit_var(a_list, e_list, i_list, time)
